12d44a17f8c4f337126dcc6a7594ea0eecedc61c
Azahara Martinez <azaharamaga@gmail.com>
2019-12-20 09:01:21 +0100

    Add docs folder

---
+++ docs/precache-manifest.ca460c961d604be19080bcd83d40bc45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e9e80e03dd05a40d50e153b51e6a607",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a030b6ac3ed7cfb5e853",
+    "url": "./static/css/main.08163736.chunk.css"
+  },
+  {
+    "revision": "dfeabebeee83e32a9f13",
+    "url": "./static/js/2.fb9071f9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.fb9071f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a030b6ac3ed7cfb5e853",
+    "url": "./static/js/main.5a96b2c9.chunk.js"
+  },
+  {
+    "revision": "a757e8854860e7757b23",
+    "url": "./static/js/runtime-main.9b7fd7af.js"
+  }
+]);
