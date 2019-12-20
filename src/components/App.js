0bbfd0bfa5e5a6e33b4850a64406b5361ce92f20
Azahara Martinez <azaharamaga@gmail.com>
2019-12-20 09:41:20 +0100

    Change App dummy to class

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,26 @@ import data from "../api/api.json";
 import PokeList from "./PokeList";
 import Pokemon from "./Pokemon";
 
-function App() {
-  return (
-    <div className="app">
-      <h1 className="app__title">Mi lista de pokemon</h1>
-      <PokeList>
-        {data.map(item => {
-          return <Pokemon key={item.id} name={item.name} image={item.url} info={item.types} />;
-        })}
-      </PokeList>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: data
+    };
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <h1 className="app__title">Mi lista de pokemon</h1>
+        <PokeList>
+          {this.state.data.map(item => {
+            return <Pokemon key={item.id} name={item.name} image={item.url} info={item.types} />;
+          })}
+        </PokeList>
+      </div>
+    );
+  }
 }
 
 export default App;
